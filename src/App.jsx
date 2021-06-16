e8bc19a3d6503169b6af451e93b21a67202d21ca
AlexisVS <63750707+AlexisVS@users.noreply.github.com>
2021-06-17 00:55:52 +0200

    base

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render () {
+    const bonjour = "Bonjour à tous"
+    return (
+      <div>
+        <h1>{bonjour}</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
